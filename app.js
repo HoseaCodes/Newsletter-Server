479afeb2257255bfbfb0ff8e1ea18f8a14e5088d
hoseacodes <dominique11h@yahoo.com>
2021-02-07 18:43:22 -0600

    Added Heroku

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.post("/failure", function (req, res) {
     res.redirect("/")
 })
 
-app.listen(port, function () {
+app.listen(process.env.PORT || port, function () {
     console.log(`Server is running on port ${port}`)
 })
 
